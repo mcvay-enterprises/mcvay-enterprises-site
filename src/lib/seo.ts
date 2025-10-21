94ea287326532cf2a11d75ca25c131e9e6fd8b5b
mcvayai <rbvhkx9ym8@privaterelay.appleid.com>
2025-10-21 11:13:31 -0400

    Create seo.ts

---
+++ src/lib/seo.ts
@@ -0,0 +1,7 @@
+export const siteMetadata = {
+  title: 'McVay Enterprises',
+  description: 'Enterprise solutions that scale with your business needs',
+  siteUrl: 'https://mcvay-enterprises.com',
+  author: 'McVay Enterprises',
+  keywords: ['enterprise', 'solutions', 'business', 'technology', 'software'],
+}
